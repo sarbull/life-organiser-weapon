0def482c780316450b3b37d22fb58877f0e28a1e
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2013-11-22 22:39:42 +0200

    Added task model

--- app/db/schema.rb
+++ app/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131122203555) do
+ActiveRecord::Schema.define(version: 20131122203850) do
+
+  create_table "tasks", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
