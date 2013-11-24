d49f2436c8047be060e3da73db544fa6a72d8b62
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2013-11-24 10:39:24 +0200

    Added json render for '/project/:id'

--- app/app/controllers/project_controller.rb
+++ app/app/controllers/project_controller.rb
@@ -12,6 +12,12 @@ class ProjectController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json {
+        render :json => @project.to_json
+      }
+    end
   end
 
 end
